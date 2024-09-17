3450f8afab1efc423dcde044ca7ffc27f0e01230
Malongmak <j.malong@alustudent.com>
2024-09-17 12:08:21 +0000

    3-graph_delete.c

---
+++ graphs/3-graph_delete.c
@@ -0,0 +1,32 @@
+#include "graphs.h"
+
+/* free */
+#include <stdlib.h>
+
+/**
+ * graph_delete - completely deletes a graph
+ *
+ * @graph: pointer to the graph to delete
+ */
+void graph_delete(graph_t *graph)
+{
+	vertex_t *temp1_v = NULL, *temp2_v = NULL;
+	edge_t *temp1_e = NULL, *temp2_e = NULL;
+
+	if (!graph)
+		return;
+
+	temp1_v = graph->vertices;
+	while (temp1_v)
+	{
+		temp2_e = temp1_e->next;
+		free(temp1_e);
+		temp1_e = temp2_e;
+	}
+	
+	free(temp1_v);
+	temp1_v = temp2_v;
+}
+
+free(graph);
+}
